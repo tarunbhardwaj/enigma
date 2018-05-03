c06a08ab5aee782277543329130ddfbe97f3cecc
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2018-05-03 10:10:48 +0530

    better UX

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+
 from flask import Flask, render_template, request, abort
 from redis import Redis
 
@@ -9,14 +10,19 @@ redis_client = Redis.from_url(os.environ['REDIS_URL'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    encr_text = request.form.get('encr')
-    if request.method == 'POST' and encr_text:
-        if len(encr_text) > 2000:
-            abort(400)
-        key = hashlib.sha224(encr_text.encode('utf-8')).hexdigest()
-        redis_client.setex(key, encr_text, 3600 * 24 * 3)
-        return key
     return render_template('index.html')
+
+
+@app.route('/encode', methods=['POST'])
+def encode():
+    encr_text = request.form.get('encr')
+    if not encr_text:
+        abort(400)
+    if len(encr_text) > 2000:
+        abort(400, 'Too large payload.')
+    key = hashlib.sha224(encr_text.encode('utf-8')).hexdigest()
+    redis_client.setex(key, encr_text, 3600 * 24 * 3)  # 3d
+    return key
 
 
 @app.route('/decode', methods=['POST'])
